1d2a8453e16132cec760bb6d7551a7f9d13087c2
Evgeniy <kozenokevgeniy@gmail.com>
2025-09-01 13:15:52 +0300

    Update plugin skeleton with plugin name

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-MyPluginModule::Engine.routes.draw do
+DiscourseCategoryLanguage::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw { mount ::DiscourseCategoryLanguage::Engine, at: "discourse-category-language" }
--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
+# name: discourse-category-language
 # about: TODO
 # meta_topic_id: TODO
 # version: 0.0.1
@@ -8,13 +8,13 @@
 # url: TODO
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :discourse_category_language_enabled
 
-module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+module ::DiscourseCategoryLanguage
+  PLUGIN_NAME = "discourse-category-language"
 end
 
-require_relative "lib/my_plugin_module/engine"
+require_relative "lib/discourse_category_language/engine"
 
 after_initialize do
   # Code which should run after Rails has finished booting
