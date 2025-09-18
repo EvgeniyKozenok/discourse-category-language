066c4c0a0ab493898ab2c38c2665c201833f17fc
Evgeniy <kozenokevgeniy@gmail.com>
2025-09-18 18:27:30 +0300

    restore admin view

--- app/models/discourse_category_language/language.rb
+++ app/models/discourse_category_language/language.rb
@@ -8,3 +8,18 @@ module DiscourseCategoryLanguage
     validates :slug, presence: true, uniqueness: true
   end
 end
+
+# == Schema Information
+#
+# Table name: plugin_category_languages
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  slug       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_plugin_category_languages_on_slug  (slug) UNIQUE
+#
