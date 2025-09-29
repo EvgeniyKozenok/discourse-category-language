ee9772f4beaa2bb85e1134a0b0b49cc98909d11a
Evgeniy <kozenokevgeniy@gmail.com>
2025-09-29 20:04:06 +0300

    mount routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,24 +2,24 @@
 # config/routes.rb
 
 DiscourseCategoryLanguage::Engine.routes.draw do
-  get "/spa-meta/:id/:entity" => "public_category_language#spa_meta"
+  get "/" => "/admin/plugins#index"
+  get "/list" => "admin/admin_category_language#list"
+  post "/" => "admin/admin_category_language#create"
+  patch "/:id" => "admin/admin_category_language#update"
+  delete "/:id" => "admin/admin_category_language#destroy"
 
-  scope "/admin", constraints: StaffConstraint.new do
-    get "/" => "/admin/plugins#index"
-    get "/list" => "admin/admin_category_language#list"
-    post "/" => "admin/admin_category_language#create"
-    patch "/:id" => "admin/admin_category_language#update"
-    delete "/:id" => "admin/admin_category_language#destroy"
+  get "/categories/:id" => "admin/admin_category_relations#index"
+  patch "/categories/:id" => "admin/admin_category_relations#update"
 
-    get "/categories/:id" => "admin/admin_category_relations#index"
-    patch "/categories/:id" => "admin/admin_category_relations#update"
-
-    get "/topics/:id/alternates" => "admin/admin_topic_relations#alternates"
-    patch "/topics/:id/alternates_update" => "admin/admin_topic_relations#alternates_update"
-    patch "/topics/:id/:x_default/assign_default" => "admin/admin_topic_relations#assign_default"
-  end
+  get "/topics/:id/alternates" => "admin/admin_topic_relations#alternates"
+  patch "/topics/:id/alternates_update" => "admin/admin_topic_relations#alternates_update"
+  patch "/topics/:id/:x_default/assign_default" => "admin/admin_topic_relations#assign_default"
 end
 
 Discourse::Application.routes.draw do
-  mount ::DiscourseCategoryLanguage::Engine, at: "/discourse-category-language"
+  get "/discourse-category-language/spa-meta/:id/:entity" => "discourse_category_language/public_category_language#spa_meta"
+
+  scope "/admin/discourse-category-language", constraints: StaffConstraint.new do
+    mount ::DiscourseCategoryLanguage::Engine, at: "/"
+  end
 end
